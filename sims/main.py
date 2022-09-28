29bd2ef1b2b2533a01fdb5998f64cbd6ca1c803d
Dwaipayan-R-C <dwaipayan0502@gmail.com>
2022-09-28 19:09:58 +0530

    added shear rate vs viscosity

--- sims/main.py
+++ sims/main.py
@@ -2,8 +2,22 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from sims import eos
+from sims import viscLearn
+import GPy
 
 def eos_active_learning(save_every,threshold,time_steps,N_init,path,N, kernel, iter_num):
+    """Main function for Active learning of equation of state
+
+    Args:
+        save_every (int): Intervals to save plots
+        threshold (double): variance allowance
+        time_steps (int): last temperature profile count
+        N_init (int): initial no. of training data points
+        path (string): path of the data folder
+        N (int): resolution
+        kernel (GPy.Kern): Kernel that decides the Cov and mean
+        iter_num (int): to save space for Y_Var
+    """
     data_path_max = f"{path}\\densitiy-vs-x\\density_t{time_steps:02d}.dat"
     data_path_min = f"{path}\\densitiy-vs-x\\density_t{0:02d}.dat"
     x, dens = np.loadtxt(data_path_max, unpack=True)
@@ -17,3 +31,20 @@ def eos_active_learning(save_every,threshold,time_steps,N_init,path,N, kernel, i
     os.makedirs = True
     fig.savefig(path)
     
+    
+def stress_viscosity_learning():
+    x_low = 0
+    x_high = 1.2e1
+    N = 100
+    N_init = 5
+    # 1.2e1 for stress vs viscosity
+    X_grid = np.linspace(x_low, x_high, 100)    
+    threshold = 1e-5
+    Number_of_iterations = 100
+    kernel = GPy.kern.RBF(1) * GPy.kern.RatQuad(1)
+    scale = 'linear'
+    x_label = r"shear stress"
+    y_label = 'shear rate'
+    
+    index_list = list(np.concatenate((np.array([x_low]), np.random.randint(x_low+1, N//2, N_init))))
+    viscLearn.active_learning_visc(X_grid,index_list,threshold,Number_of_iterations,kernel, scale, x_label, y_label)
---
+++ sims/viscLearn.py
@@ -0,0 +1,69 @@
+import os
+import numpy as np
+from argparse import ArgumentParser
+import matplotlib.pyplot as plt
+from utils import TFuncs
+from utils import GPLearn
+from utils import plots
+
+
+def active_learning_visc(X_grid,index_list,threshold,Number_of_iterations,kernel, scale, x_label, y_label ):
+    N_init = len(index_list)
+    
+    X_samples = X_grid[index_list]
+    print(X_samples.shape)
+
+    Y_samples = TFuncs.target_stress(X_samples,0.5,2,.5,.2,4)
+    Y_grid = TFuncs.target_stress(X_grid, 0.5,2,.5,.2,4)   
+        
+    # pseudo 2D shape for GPs
+    X_samples = np.array(X_samples)[:, None]
+    Y_samples = np.array(Y_samples)[:, None]
+    X_grid = np.array(X_grid)[:, None]
+    
+    # We will use this matrix to store the GP mean at every iteration.
+    Y_estimates = np.full((len(X_grid), Number_of_iterations + 1), np.nan)
+    varY_estimates = np.full((len(X_grid), len(X_grid), Number_of_iterations + 1), np.nan)
+
+    # GP regression
+    mean, Cov, variance, m = GPLearn.GP_analysis_viscosity(X_samples, Y_samples, X_grid,kernel)
+    
+    # Store the results to plot later
+    Y_estimates[:, 0] = mean.ravel()
+    varY_estimates[:, :, 0] = Cov
+    num_list=[]
+    i = 0
+    while(np.max(variance)>threshold):
+        
+        # find the next sample
+        next_sample_index = np.argmax(variance)
+        print(np.max(variance))
+
+        index_list.append(next_sample_index)
+        next_sample_loc = X_grid[next_sample_index]
+
+        next_sample_value = TFuncs.target_stress(next_sample_loc, 0.5,2,.5,.2,4)
+        
+
+        # add the desired sample to our data
+        X_samples = np.vstack((X_samples, X_grid[next_sample_index, :]))
+        Y_samples = np.vstack((Y_samples, np.array([next_sample_value])[0, None]))
+
+        # GP regression
+        mean, Cov, variance, m = GPLearn.GP_analysis_viscosity(X_samples, Y_samples, X_grid, kernel)  
+
+        Y_estimates[:, i+1] = mean.ravel()
+        varY_estimates[:, :, i+1] = Cov
+        if(i%2==0):
+            num_list.append(i)
+        i = i+1
+    Number_of_iterations = i
+    index_list.append(0)
+    plots.plot_summary_visc(Number_of_iterations ,
+                 X_grid,
+                 X_samples,
+                 Y_samples,
+                 index_list,
+                 Y_estimates,
+                 varY_estimates, Y_grid, N_init, num_list,
+                 scale, x_label, y_label)
